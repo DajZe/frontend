284e5bbb3763496e79c6b89c8d031c35efa20c4c
daje <nindyrizkyashafarra@gmail.com>
2024-07-10 11:03:00 +0700

    tambahan kotak1 dan 2

---
+++ frontend/src/components/Kotak1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Kotak1() {
+    return (
+      <>
+        <h3>Kotak 1</h3>
+  
+      </>
+    )
+  }
+  
+  export default Kotak1
---
+++ frontend/src/components/Kotak2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Kotak2() {
+    return (
+      <>
+        <h2>Kotak 2</h2>
+      </>
+    )
+  }
+  
+  export default Kotak2
